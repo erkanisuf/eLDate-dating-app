41473b47cdd408eb13ac645a94b3de25a1a8d955
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2021-01-30 20:43:54 +0200

    deploying

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "need226090",
-  host: "localhost",
-  port: 5432,
-  database: "dateapp",
+  user: process.env.DATABASEPG_USER,
+  password: process.env.DATABASEPG_PASSWORD,
+  host: process.env.DATABASEPG_HOST,
+  port: process.env.DATABASEPG_PORT,
+  database: process.env.DATABASEPG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
