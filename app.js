b408ff6fad22c558a936e4fa69ee0e7d33867a7f
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2021-01-30 21:54:29 +0200

    'bugsessions'

--- app.js
+++ app.js
@@ -82,9 +82,14 @@ app.get("/getcookie", (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello and Welcome!!");
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+// app.get("/", (req, res) => {
+//   res.send("Hello and Welcome!!");
+// });
 // -========End routes
 
 app.listen(port, () => {
